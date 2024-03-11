f6d0d4ee5871c2b74f726b87d12a4a796e75efcf
lakshitrajput <lakshit1729@gmail.com>
2024-03-11 10:34:07 +0530

    Added post schema

---
+++ backend/Model/PostModel.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.schema({
+        
+
+})
+
+
+const postModel = mongoose.model("postModel", postSchema);
+
+module.exports = postModel;
