b3c9579a8d3189d7a0446d4f08c373f24b93877d
lakshitrajput <lakshit1729@gmail.com>
2024-03-19 19:25:22 +0530

    Minor fix

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const bodyParser=require('body-parser');
 const path = require('path');
 
 const app=express();
-const path = require('path');
+
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended:false}))
